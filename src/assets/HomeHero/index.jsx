392dea0b3cbbfd4bb035d3c785650638f51cd037
tugaycan <tcduymaz94@gmail.com>
2023-12-28 22:10:40 +0300

    added logos

---
+++ src/assets/HomeHero/index.jsx
@@ -0,0 +1,4 @@
+export { default as Home1 } from "./home1.jpg"
+export { default as Home2 } from "./home2.jpg"
+export { default as Home3 } from "./home3.jpg"
+export { default as Home4 } from "./home4.jpg"
