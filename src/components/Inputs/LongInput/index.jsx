caf42d3438f40b79850ba562b0a6ede930379aa7
tugaycan <tcduymaz94@gmail.com>
2024-01-04 14:38:46 +0300

    created, used and added

--- src/components/Inputs/LongInput/index.jsx
+++ src/components/Inputs/LongInput/index.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
 
-const LongInput = () => {
+
+const LongInput = ({ content }) => {
     return (
-        <div>
+        <div className=' w-[100%]'>
+            <div className='relative hero-rightSide-input mb-4'>
+                <input
+                    required
+                    className='border-2 
+                    border-input
+                    rounded-[4px]
+                    bg-[#fff]
+                    w-[100%]
+                    h-[56px]
+                    text-[14px]
+                    text-[#191918]
+                    hover:border-hoverInput
+                    focus:border-hoverInput
+                    focus-visible:none
+                    outline-none
+                    px-[14px]
+                    peer
+                    focus:pt-[5px]
+                    valid:pt-[5px]
+                    
+                    '/>
+                <label className="
+                text-[#838287]
+                absolute
+                left-[14px]
+                translate-y-[-50%]
+                top-[50%]
+                text-[14px]
+                leading-[16px]
+                pointer-events-none
+                transition-all
+                peer-focus:text-[12px]
+                peer-focus:text-[#5d3ebc]
+                peer-focus:translate-y-0
+                peer-focus:top-[4px]
 
+                peer-valid:text-[12px]
+                peer-valid:text-[#5d3ebc]
+                peer-valid:translate-y-0
+                peer-valid:top-[4px]
+                ">{content}</label>
+            </div>
         </div>
     )
 }
