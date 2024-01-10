025840a7b87f185924e246230a5c09677d822e74
tugaycan <tcduymaz94@gmail.com>
2024-01-10 15:40:23 +0300

    remove fixed header for small screen

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,7 +6,7 @@ import "./style.css"
 
 const Header = () => {
     return (
-        <div className="fixed top-0 z-20 w-full">
+        <div className="md:fixed top-0 z-20 w-full">
             <nav className="h-header bg-header text-[#fff] md:px-4 lg:px-[32px] px-[6px] flex items-end overflow-x-auto">
                 <div className="flex justify-between w-[100%] max-w-[1232px] mx-auto">
                     <div className="md:block flex items-end ">
