04fd853703533b1c787e97b5a40190fa174f3a5b
tugaycan <tcduymaz94@gmail.com>
2024-01-10 15:35:24 +0300

    added fixed header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,18 +6,20 @@ import "./style.css"
 
 const Header = () => {
     return (
-        <nav className="h-header bg-header text-[#fff] md:px-4 lg:px-[32px] px-[6px] flex items-end overflow-x-auto">
-            <div className="flex justify-between w-[100%] max-w-[1232px] mx-auto">
-                <div className="md:block flex items-end ">
-                    <Navigations />
+        <div className="fixed top-0 z-20 w-full">
+            <nav className="h-header bg-header text-[#fff] md:px-4 lg:px-[32px] px-[6px] flex items-end overflow-x-auto">
+                <div className="flex justify-between w-[100%] max-w-[1232px] mx-auto">
+                    <div className="md:block flex items-end ">
+                        <Navigations />
+                    </div>
+                    <div className="md:flex hidden">
+                        <Language />
+                        <Login />
+                        <Register />
+                    </div>
                 </div>
-                <div className="md:flex hidden">
-                    <Language />
-                    <Login />
-                    <Register />
-                </div>
-            </div>
-        </nav>
+            </nav>
+        </div>
     )
 }
 
